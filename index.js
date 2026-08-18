4b99325aca584ded5022e9d95b29378d299a15ab
demo <bsatishsrinivas@gmail.com>
2026-08-18 13:20:10 +0530

    code io print conflict

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-console.log("welcome to the git course")
-
-for(var i=0; i<5; i++){
-    console.log("code ");
-=======
 console.log("welcome to git playlist");
 for(var i=0;i<5;i++){
     console.log("code io");
->>>>>>> main
 }
